1a7fc3bce65ae68a01d35be832657cfb831e3362
pchalupa <chalupa.petr93@gmail.com>
2026-01-01 13:01:38 +0100

    Fix shebang

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-#!/bin/env node
+#!/usr/bin/env node
 
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
